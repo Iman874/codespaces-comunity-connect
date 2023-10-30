a9d74e713b4310fdce34b39d8684c7c8ebfdc0a2
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2023-10-30 13:02:37 +0000

    fd

--- conn.js
+++ conn.js
@@ -3,26 +3,33 @@ const mysql = require('mysql');
 // Konfigurasi koneksi MySQL
 const connection = mysql.createConnection({
   host: "localhost", // Ganti dengan host MySQL Anda
-    user: "root", // Ganti dengan username MySQL Anda
-      password: "" // Ganti dengan password MySQL Anda
-      });
+  user: "root", // Ganti dengan username MySQL Anda
+  password: "" // Ganti dengan password MySQL Anda
+});
 
-      // Membuat koneksi ke MySQL
-      connection.connect((err) => {
-        if (!err) {
+// Membuat koneksi ke MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Koneksi ke MySQL gagal: ' + err.stack);
+    return;
+  }
+  console.log('Terhubung ke MySQL dengan ID ' + connection.threadId);
 
-            console.error('Koneksi ke MySQL gagal: ' + err.stack);
-                return;
-                  }
-                    console.log('Terhubung ke MySQL dengan ID ' + connection.threadId);
-                    });
+  // Membuat database "testing"
+  connection.query('CREATE DATABASE testing', (err, results) => {
+    if (err) {
+      console.error('Gagal membuat database "testing": ' + err.message);
+    } else {
+      console.log('Database "testing" berhasil dibuat');
+    }
 
-                    // Menutup koneksi setelah selesai
-                    connection.end((err) => {
-                      if (!err) {
-                          console.error('Error dalam menutup koneksi: ' + err.message);
-                              return;
-                                }
-                                  console.log('Koneksi MySQL ditutup');
-                                  });
-                                
+    // Menutup koneksi setelah selesai
+    connection.end((err) => {
+      if (err) {
+        console.error('Error dalam menutup koneksi: ' + err.message);
+        return;
+      }
+      console.log('Koneksi MySQL ditutup');
+    });
+  });
+});
