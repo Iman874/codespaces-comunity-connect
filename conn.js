210ac2965c3f054711067c79040843c7a8359efc
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2023-10-30 09:19:41 +0000

    conec to mysql

---
+++ conn.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+
+// Konfigurasi koneksi MySQL
+const connection = mysql.createConnection({
+  host: 'localhost', // Ganti dengan host MySQL Anda
+    user: 'root', // Ganti dengan username MySQL Anda
+      password: '' // Ganti dengan password MySQL Anda
+      });
+
+      // Membuat koneksi ke MySQL
+      connection.connect((err) => {
+        if (err) {
+            console.error('Koneksi ke MySQL gagal: ' + err.stack);
+                return;
+                  }
+                    console.log('Terhubung ke MySQL dengan ID ' + connection.threadId);
+                    });
+
+                    // Menutup koneksi setelah selesai
+                    connection.end((err) => {
+                      if (err) {
+                          console.error('Error dalam menutup koneksi: ' + err.message);
+                              return;
+                                }
+                                  console.log('Koneksi MySQL ditutup');
+                                  });
