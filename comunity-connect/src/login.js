50b308a6fa9f00ba47ca54f4aa29af9121f6dacf
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2023-11-04 06:49:03 +0000

    Update login

--- comunity-connect/src/login.js
+++ comunity-connect/src/login.js
@@ -11,22 +11,37 @@ function Login() {
         login_cek(email, password);
     }
 
-     (
-        <main>
-            <h2>Login</h2>
-            <form onSubmit={btnSubmit}>
-                <label>Email:
-                 return   <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </label>
-                <br />
-                <label>Password:
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-                <br/>
-                <button type='submit' >Login</button>
-            </form>
-            <p>Don't have an account? <a href="google.com">Sign Up</a></p>
-        </main>
+    return (
+        <div class = "halaman">
+            <div class="photo"><img src="forlogin.jpg"></img></div>
+            <div class = "logincontainer">
+                <div>
+                    <span>Welcome to<br/></span>
+                    <div class = "fontwlc2">Community Connect</div>
+                </div>
+                <div class = "colomlogin"></div>
+                <div class = "colomlogin"></div>
+                <div class = "checkuser"></div>
+                <div class = "colomlogin"></div>
+                <div class="toregister">
+                    <span>Don’t have an account? </span>
+                    <div class="toregisterfont2">Register</div>
+                </div>
+                <h2>Login</h2>
+                <form onSubmit={btnSubmit}>
+                    <label>Email:
+                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </label>
+                    <br />
+                    <label>Password:
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                    <br/>
+                    <button type='submit' >Login</button>
+                </form>
+                <p>Don't have an account? <a href="google.com">Sign Up</a></p>
+            </div>
+        </div>
     );
 }
 
